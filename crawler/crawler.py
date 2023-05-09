8c308c911d400b252f714129cd8342b2019956d6
Serdar AKYOL <serdarakyol55@outlook.com>
2023-05-09 12:51:26 +0200

    Extracted points

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,38 +1,52 @@
-from typing import Any
 from requests import get
 from .base_crawler import BaseCrawler
 from bs4 import BeautifulSoup
-# title
+from .enum import DataEntry
 # number of the order
 # number of comments
-# points
 class CustomCrawler(BaseCrawler):
 
     def __init__(self, link: str, page: int) -> None:
         super().__init__(link, page)
         self.result = {
-            "Title": list[str],
-            "Rank": list[int],
-            "TotalCommand": list[int],
-            "Points": list[int]
+            DataEntry.Title.value: list[str],
+            DataEntry.Point.value: list[int],
+            DataEntry.Rank.value: list[int],
+            DataEntry.TotalCommand.value: list[int]
         }
-    
-    def extract_titles(self, content) -> int:
-        soup = BeautifulSoup(content, 'lxml')
-        td_tags = soup.select('span.titleline > a')
-        
-        # best practice for memory allocation
-        all_titles = list(td.get_text(strip=True) for td in td_tags)
-        self.result["Title"] = all_titles
-        
-        if len(all_titles) == 30:
-            print("Titles are succesfully extracted")
+
+    def extract_and_check(self, tags:BeautifulSoup, type:str) -> None:
+        match type:
+            case DataEntry.Title:
+                all_tags = list(tg.get_text(strip=True) for tg in tags)
+            case DataEntry.Point:
+                all_tags = list(int(tg.get_text(strip=True).replace(" points", "")) for tg in tags)
+            case DataEntry.Rank:
+                print("RANK")
+            case DataEntry.TotalCommand:
+                print("TOTAL COMMAND")
+
+        # check if all tags extracted
+        if len(all_tags) == 30:
+            print(f"{type.value} Successfully extracted")
         else:
-            print("All titles are not extracted successfuly")
+            print(f"All {type.value} could not extract")
+        self.result[type.value] = all_tags
+    
+    def extract_titles(self, soup:BeautifulSoup) -> None:
+        span_tags = soup.select('span.titleline > a')
+        self.extract_and_check(span_tags, DataEntry.Title)
+        
+    def extract_points(self, soup:BeautifulSoup) -> None:
+        span_tags = soup.find_all('span', class_='score')
+        self.extract_and_check(span_tags, DataEntry.Point)
 
     def start(self):
         response = get(self.link) 
         if response.status_code == 200:
-            self.extract_titles(response.text)
+            soup = BeautifulSoup(response.text, 'lxml')
+            self.extract_titles(soup)
+            self.extract_points(soup)
         else: 
             print("FAIL", response.status_code)
+        print(self.result)
---
+++ crawler/enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class DataEntry(Enum):
+    Title = "Title"
+    Point = "Point"
+    Rank = "Rank"
+    TotalCommand = "Total Command"
