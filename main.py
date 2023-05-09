57af0266c7bb9669d2a873f9b96e17c537d8f358
Serdar AKYOL <serdarakyol55@outlook.com>
2023-05-09 16:37:39 +0200

    Order extracted (#4)

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from crawler.crawler import CustomCrawler
+from utils.extract_file import JsonToCsv
 from argparse import ArgumentParser
-
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('-l', '--link', help='Link of website')
+    parser.add_argument('-l', '--link', help='Link of website', required=True)
     parser.add_argument('-p', '--page', help='Number of page')
+    parser.add_argument('-o', '--output', help='Output file directory')
+    parser.add_argument('-f', '--filename', help='Name of the file to be generated')
     
     args = parser.parse_args()
 
     crawler = CustomCrawler(args.link, args.page)
-    crawler.start()
+    json_object = crawler.start()
+    saver = JsonToCsv(json_object, args.output, args.filename)
+    saver.generate_csv()
     
---
+++ utils/extract_file.py
@@ -0,0 +1,19 @@
+import csv
+from os import getcwd
+
+class JsonToCsv:
+    def __init__(self, json_object:dict, output_dir:str, filename:str = "output.csv") -> None:
+        self.json_object = json_object
+        self.output_dir = output_dir
+        self.filename = filename
+        if output_dir is None:
+            self.output_dir = getcwd()
+
+    def generate_csv(self) -> None:
+        field_names = list(self.json_object.keys())
+        rows = zip(self.json_object["Title"], self.json_object["Point"], self.json_object["Rank"], self.json_object["Total Command"])
+        with open(f"{self.output_dir}/{self.filename}", "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(field_names)
+            writer.writerows(rows)
+        print(f"Data has been generated successfully and saved to {self.output_dir}/{self.filename}")
