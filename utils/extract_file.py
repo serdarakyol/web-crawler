af401b52f6d9b9e5738aaa48149447bb0232d49a
Serdar AKYOL <serdarakyol55@outlook.com>
2023-05-09 17:46:58 +0200

    add ci (#6)

--- utils/extract_file.py
+++ utils/extract_file.py
@@ -2,14 +2,17 @@ import csv
 from os import getcwd
 
 class JsonToCsv:
-    def __init__(self, json_object:dict, output_dir:str, filename:str = "output.csv") -> None:
+    def __init__(self, json_object: dict, output_dir: str, filename: str) -> None:
         self.json_object = json_object
         self.output_dir = output_dir
         self.filename = filename
         if output_dir is None:
             self.output_dir = getcwd()
+        if filename is None:
+            self.filename = "output.csv"
 
     def generate_csv(self) -> None:
+        print(self.filename)
         field_names = list(self.json_object.keys())
         rows = zip(self.json_object["Title"], self.json_object["Point"], self.json_object["Rank"], self.json_object["Total Command"])
         with open(f"{self.output_dir}/{self.filename}", "w", newline="") as file:
